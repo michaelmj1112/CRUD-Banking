a00cdbd089aa9f7d99903fd941eeb9c6f5d72bc5
Michael Jordan <75946408+michaelmj1112@users.noreply.github.com>
2023-01-12 15:10:03 +0800

    FIX - client profile back button fix

--- index.php
+++ index.php
@@ -14,6 +14,8 @@
         <div class="alert alert-<?=$_SESSION['msg_type']?>">
             <?php
                 echo $_SESSION['message'];
+
+
                 unset($_SESSION['message']);
             ?>
         </div>
